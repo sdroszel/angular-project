7f80259a08db0cc48e093ddb90774dd600c16c86
Scott Roszel <57762591+sdroszel@users.noreply.github.com>
2024-08-17 12:59:51 -0400

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstCompComponent
   ],
   imports: [
     BrowserModule,
